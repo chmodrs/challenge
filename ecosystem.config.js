f45dcef5ae355e8df8dd2cfee421f1e860395271
chmodrs <mauriciorocha.rgs@gmail.com>
2017-11-21 23:45:03 +0000

    add pm2 file

---
+++ ecosystem.config.js
@@ -0,0 +1,52 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps : [
+
+    // First application
+    {
+      name      : 'API',
+      script    : 'cluster.js',
+      env: {
+        COMMON_VARIABLE: 'true'
+      },
+      env_production : {
+        NODE_ENV: 'production'
+      }
+    },
+
+    // Second application
+    {
+      name      : 'WEB',
+      script    : 'web.js'
+    }
+  ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  deploy : {
+    production : {
+      user : 'node',
+      host : 'localhost',
+      ref  : 'origin/master',
+      repo : 'https://github.com/chmodrs/challenge.git',
+      path : '/root/challenge',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+    },
+    dev : {
+      user : 'node',
+      host : 'localhost',
+      ref  : 'origin/master',
+      repo : 'https://github.com/chmodrs/challenge.git',
+      path : '/root/challenge',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env dev',
+      env  : {
+        NODE_ENV: 'dev'
+      }
+    }
+  }
+};
