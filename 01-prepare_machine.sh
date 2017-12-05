c3d4f7e178e8937f5c7ccce464057ae551721505
chmodrs <mauriciorocha.rgs@gmail.com>
2017-12-05 00:54:34 +0000

    changes

---
+++ 01-prepare_machine.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+### UPDATE UBUNTU REPOSITORY ###
+
+apt-get update -y
+
+### UPGRADE S.O PACKAGES ###
+
+apt-get upgrade -y
+
+### INSTALL BUILD ESSENTIALS FOR COMPILE NODE ###
+
+apt-get install -y build-essential
+
+### DOWNLOAD AND INSTALL LATEST NODEJS VERSION ###
+
+cd /usr/src
+wget https://nodejs.org/dist/v8.9.1/node-v8.9.1.tar.gz
+tar xvf node-v8.9.1.tar.gz
+cd node-v8.9.1
+./configure && make && make install
+cd ~
+
+### CREATE SIMLINK ###
+ln -s /usr/local/bin/npm /usr/bin/npm
+
+### CREATE DEPLOY USER ###
+
+useradd -s /bin/bash -m -d /home/deploy -c "deploy user" deploy
+echo deploy:challenge2017 | chpasswd
+usermod -aG sudo deploy 
+
+### INSTALL PM2 FOR SECURE DEPLOY ###
+
+npm install pm2 -g 
